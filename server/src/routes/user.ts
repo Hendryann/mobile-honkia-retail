d292fc7b7b53ccaa503402e1c7de80526edd7ad6
frostice482 <frostice48260@gmail.com>
2026-05-12 09:50:39 +0700

    add register/login

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,4 +1,8 @@
+import { hashPassword } from '../auth'
+import db from '../db'
 import * as server from '../server'
+import express from 'express'
+import jwt from 'jsonwebtoken'
 
 server.app.get('/userinfo',
 	server.needAuth,
@@ -8,15 +12,27 @@ server.app.get('/userinfo',
 )
 
 server.app.post('/login',
-	(req, res) => {
-		console.warn('unimplemented POST /login')
-		res.end()
-	}
+	express.json({ limit: '3kb', type: () => true }),
+	server.jsonSchemaValidate(await import('../schema/login.json')),
+	server.promisifyHandler(async (req, res) => {
+		const [[user]] = await db.execute('select name, isadmin from users where name = ? and password = ?', [req.body.username, hashPassword(req.body.username, req.body.password)]) as any
+		if (!user) return res.status(401).end()
+
+		const token = jwt.sign(user, process.env.jwt_secret!)
+		res.json({ token }).end()
+	})
 )
 
 server.app.post('/register',
-	(req, res) => {
-		console.warn('unimplemented POST /register')
-		res.end()
-	}
+	express.json({ limit: '3kb', type: () => true }),
+	server.jsonSchemaValidate(await import('../schema/login.json')),
+	server.promisifyHandler(async (req, res) => {
+		const [[exists]] = await db.execute('select name from users where name = ?', [req.body.username]) as any
+		if (exists) return res.status(400).send('User already exist').end()
+
+		await db.execute('insert into users(name, password) values(?, ?)', [req.body.username, hashPassword(req.body.username, req.body.password)])
+
+		const token = jwt.sign({ name: req.body.username, isadmin: false }, process.env.jwt_secret!)
+		res.json({ token }).end()
+	})
 )
